d779897e8bb77c2e272c90744328b8ca4a3d81bd
yuqing <yuqingqiu93@gmail.com>
2018-01-24 23:09:05 -0800

    updated variable names

--- Stack/Stack/Stack.hpp
+++ Stack/Stack/Stack.hpp
@@ -4,11 +4,11 @@ using namespace std;
 
 class Stack {
 	// e)
-	int s[10]; //should the constexpr go here? it has to be defined to become a constexpr isnt it
-	int t;
+	int elements[10]; //should the constexpr go here? it has to be defined to become a constexpr isnt it
+	int topPoint;
 public:
 	// f) constructor
-	Stack() : t(-1) {};
+	Stack() : topPoint(-1) {};
 	// g)
 	void push(int);
 	// h)
